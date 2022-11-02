39762ba1d6173b9bc63180dee71a2f93f59998d4
jadenhmansfield <91288105+jadenhmansfield@users.noreply.github.com>
2022-11-02 16:47:49 -0400

    in class exercise

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function story() {
-  const name = document.getElementById("name").value;
+  const nombre = document.getElementById("nombre").value;
   const candy = document.getElementById("candy").value;
   const costume= document.getElementById("costume").value;
-  document.getElementById("result").innerHTML = `Once upon a time, a very spooky ${name} dressed as ${costume} went trick or treating and got more ${candy} than they knew what to do with.`;
+  document.getElementById("result").innerHTML = `Once upon a time, a very spooky ${nombre} dressed as ${costume} went trick or treating and got more ${candy} than they knew what to do with.`;
 }
 
 document.getElementById('run').addEventListener('click', e => {
   story();
 })
+
+setTimeout(()=>{
+  nombre.value = '';
+  candy.value='';
+  costume.value='';
+}, 5000);
 
 console.log('its working');
